90ef4d553c2f0f798b864ff82be13e699c3bc94f
PSesharao <paritalasesharao@gmail.com>
2023-09-09 18:34:30 +0530

    add run time exception message on Sat, Sep 09, 2023   6:34:30 PM

--- order-service/src/main/java/com/seshrao/stockxpress/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/seshrao/stockxpress/orderservice/controller/OrderController.java
@@ -29,7 +29,7 @@ public class OrderController {
     }
 
     public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please order after some time!");
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please order after some time ! " + runtimeException.getMessage()  );
     }
 
 }
